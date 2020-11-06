827a655e778a9f908e4f4f7592b18fee2576de70
senkpm74 <senkpm2@gmail.com>
2020-11-06 10:02:04 +0530

    Initial Commit

---
+++ com.sen.git.first/src/com/sen/git/first/pkg/gitCode.java
@@ -0,0 +1,12 @@
+package com.sen.git.first.pkg;
+
+public class gitCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test Git package");
+
+	}
+
+}
